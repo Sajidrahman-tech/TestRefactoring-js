e1f1bc25b9a97904f6aac94f61c88f2f3df20860
Sajidrahman-tech <ararman7715@gmail.com>
2024-10-13 01:41:40 -0300

    added java file before refactoring

---
+++ refactoring.java
@@ -0,0 +1,19 @@
+
+public class refactoring {
+    public static void main(String[] args) {
+        // Create a Scanner object to read input
+
+        // Prompt the user to enter the first number
+        double firstNumber = 2
+
+        // Prompt the user to enter the second number
+        double secondNumber = 4
+
+        // Perform addition
+        double sum = firstNumber + secondNumber;
+
+        // Display the result
+        System.out.println("The sum of " + firstNumber + " and " + secondNumber + " is: " + sum);
+
+    }
+}
